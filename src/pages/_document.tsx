831fd4f91249174a1fdcfe28fc1a7317101cc10e
Jose Maza <111375069+jgmaza-infinity@users.noreply.github.com>
2024-05-10 16:30:48 -0500

    chore: Add Font Awesome script and update body styles in _document.tsx, and improve styling in index.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <script
+        src="https://kit.fontawesome.com/3915ec2c1e.js"
+        crossOrigin="anonymous"
+        async
+      ></script>
+      <body
+        style={{
+          padding: 0,
+          margin: 0,
+          boxSizing: "border-box",
+          overflowY: "hidden",
+        }}
+      >
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import Vehicle from "@/interface/vehicle";
 import { useQuery } from "@tanstack/react-query";
 import { Inter } from "next/font/google";
+
+import Style from "./style.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +21,22 @@ export default function Home() {
     },
   });
   return (
-    <main className={`bg-[#09090b] h-lvh max-h-screen scroll-auto ${inter.className}`}>
+    <main
+      className={`bg-[#09090b] h-screen max-h-screen overflow-auto relative ${inter.className} ${Style.scrollBar}`}
+    >
       <h1 className="text-white text-4xl font-bold text-center pt-10">
         Vehículos
       </h1>
       <div className="flex flex-row gap-4 p-4">
         {data?.map((vehicle: Vehicle) => (
           <Card key={vehicle.id} className="p-4 bg-[#1e1e1e] text-white w-fit">
-            <img src={vehicle.photo} alt={vehicle.name} width={300} height={200} />
+            <img
+              src={vehicle.photo}
+              alt={vehicle.name}
+              width={300}
+              height={300}
+              className={Style.images}
+            />
             <p>Nombre: {vehicle.name}</p>
             <p>Marca: {vehicle.make}</p>
             <p>Modelo: {vehicle.model}</p>
@@ -33,6 +45,10 @@ export default function Home() {
           </Card>
         ))}
       </div>
+
+      <Button className="bg-[#1e1e1e] text-white fixed bottom-5 right-5 rounded-full">
+        <i className="fas fa-plus"></i>
+      </Button>
     </main>
   );
 }
